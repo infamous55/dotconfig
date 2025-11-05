65da5ec1673a8177e846a23cf52bb02bfc99e29f
infamous55 <contact@infamous55.com>
2025-11-05 21:38:45 +0200

    add diagnostics and lsp keybindings

--- nvim/init.lua
+++ nvim/init.lua
@@ -50,110 +50,127 @@ vim.keymap.set("n", "<leader>b", "<cmd>FzfLua buffers<cr>")
 vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<cr>")
 vim.keymap.set("n", "<leader>lg", "<cmd>FzfLua live_grep<cr>")
 
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "gD", vim.lsp.buf.type_definition)
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
+vim.keymap.set("n", "gr", vim.lsp.buf.references)
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
+vim.keymap.set("n", "<space>r", vim.lsp.buf.rename)
+vim.keymap.set("n", "<space>f", vim.lsp.buf.format)
+vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action)
+
+vim.keymap.set("n", "<space>d", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", function()
+	vim.diagnostic.jump({ count = -1 })
+end)
+vim.keymap.set("n", "]d", function()
+	vim.diagnostic.jump({ count = 1 })
+end)
+
 -- Plugins
 vim.pack.add({
-    { src = "https://github.com/projekt0n/github-nvim-theme" },
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
-    { src = "https://github.com/saghen/blink.cmp",               version = vim.version.range("^1") },
-    { src = "https://github.com/mason-org/mason.nvim" },
-    { src = 'https://github.com/neovim/nvim-lspconfig' },
-    { src = "https://github.com/stevearc/conform.nvim" },
-    { src = "https://github.com/nvim-tree/nvim-web-devicons" },
-    { src = "https://github.com/nvim-tree/nvim-tree.lua" },
-    { src = "https://github.com/ibhagwan/fzf-lua" },
-    { src = "https://github.com/nvim-mini/mini.diff" },
-    { src = "https://github.com/nvim-mini/mini.comment" },
-    { src = "https://github.com/kylechui/nvim-surround" },
+	{ src = "https://github.com/projekt0n/github-nvim-theme" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/saghen/blink.cmp", version = vim.version.range("^1") },
+	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/stevearc/conform.nvim" },
+	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
+	{ src = "https://github.com/nvim-tree/nvim-tree.lua" },
+	{ src = "https://github.com/ibhagwan/fzf-lua" },
+	{ src = "https://github.com/nvim-mini/mini.diff" },
+	{ src = "https://github.com/nvim-mini/mini.comment" },
+	{ src = "https://github.com/kylechui/nvim-surround" },
 }, { load = true })
 
 vim.cmd("colorscheme github_dark_colorblind")
 
 require("nvim-treesitter.configs").setup({
-    ensure_installed = {
-        "lua",
-        "c",
-        "cpp",
-        "meson",
-        "rust",
-        "go",
-        "gomod",
-        "gosum",
-        "python",
-        "bash",
-        "sql",
-        "dockerfile",
-        "just",
-        "gitignore",
-        "markdown",
-        "json",
-        "toml",
-        "yaml",
-        "kdl",
-        "typst",
-        "diff",
-        "regex",
-    },
-    sync_install = false,
-    highlight = { enable = true },
-    indent = { enable = true },
+	ensure_installed = {
+		"lua",
+		"c",
+		"cpp",
+		"meson",
+		"rust",
+		"go",
+		"gomod",
+		"gosum",
+		"python",
+		"bash",
+		"sql",
+		"dockerfile",
+		"just",
+		"gitignore",
+		"markdown",
+		"json",
+		"toml",
+		"yaml",
+		"kdl",
+		"typst",
+		"diff",
+		"regex",
+	},
+	sync_install = false,
+	highlight = { enable = true },
+	indent = { enable = true },
 })
 
 require("blink.cmp").setup({
-    keymap = {
-        preset = "enter",
-        ["<C-u>"] = { "scroll_documentation_up", "fallback" },
-        ["<C-d>"] = { "scroll_documentation_down", "fallback" },
-    },
-    completion = {
-        documentation = {
-            auto_show = true,
-            auto_show_delay_ms = 0,
-        },
-    },
-    signature = { enabled = true },
-    cmdline = { enabled = false },
+	keymap = {
+		preset = "enter",
+		["<C-u>"] = { "scroll_documentation_up", "fallback" },
+		["<C-d>"] = { "scroll_documentation_down", "fallback" },
+	},
+	completion = {
+		documentation = {
+			auto_show = true,
+			auto_show_delay_ms = 0,
+		},
+	},
+	signature = { enabled = true },
+	cmdline = { enabled = false },
 })
 
 vim.env.PATH = vim.fn.stdpath("data") .. "/mason/bin:" .. vim.env.PATH
 require("mason").setup({})
 
 require("conform").setup({
-    formatters_by_ft = {
-        lua = { "stylua" },
-        c = { "clang-format" },
-        cpp = { "clang-format" },
-        rust = { "rustfmt" },
-        go = { "gofumpt", "goimports", "goimports-reviser", "golines" },
-        python = { "ruff_format" },
-        bash = { "shfmt" },
-        markdown = { "prettier" },
-        json = { "prettier" },
-        toml = { "taplo" },
-        yaml = { "prettier" },
-        kdl = { "kdlfmt" },
-        typst = { "typstyle" },
-    },
-    default_format_opts = {
-        lsp_format = "fallback",
-    },
-    format_on_save = { timeout_ms = 1000 },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		c = { "clang-format" },
+		cpp = { "clang-format" },
+		rust = { "rustfmt" },
+		go = { "gofumpt", "goimports", "goimports-reviser", "golines" },
+		python = { "ruff_format" },
+		bash = { "shfmt" },
+		markdown = { "prettier" },
+		json = { "prettier" },
+		toml = { "taplo" },
+		yaml = { "prettier" },
+		kdl = { "kdlfmt" },
+		typst = { "typstyle" },
+	},
+	default_format_opts = {
+		lsp_format = "fallback",
+	},
+	format_on_save = { timeout_ms = 1000 },
 })
 
 require("nvim-tree").setup({
-    filters = { custom = { "^\\.git$" } },
-    disable_netrw = true,
-    prefer_startup_root = true,
-    update_focused_file = { enable = true },
-    view = { side = "right" },
-    git = { ignore = false },
-    renderer = {
-        highlight_git = "all",
-        indent_markers = { enable = true },
-    },
+	filters = { custom = { "^\\.git$" } },
+	disable_netrw = true,
+	prefer_startup_root = true,
+	update_focused_file = { enable = true },
+	view = { side = "right" },
+	git = { ignore = false },
+	renderer = {
+		highlight_git = "all",
+		indent_markers = { enable = true },
+	},
 })
 
 require("fzf-lua").setup({
-    winopts = { backdrop = 0 },
+	winopts = { backdrop = 0 },
 })
 
 require("mini.diff").setup({})
@@ -162,14 +179,16 @@ require("nvim-surround").setup({})
 
 -- LSP
 vim.lsp.enable({
-    "lua_ls",
-    "clangd",
-    "rust_analyzer",
-    "gopls",
-    "ruff",
-    "ty",
-    "bashls",
-    "taplo",
-    "tinymist",
+	"lua_ls",
+	"clangd",
+	"rust_analyzer",
+	"gopls",
+	"ruff",
+	"ty",
+	"bashls",
+	"taplo",
+	"tinymist",
 })
 
+-- Diagnostics
+vim.diagnostic.config({ severity_sort = true })
